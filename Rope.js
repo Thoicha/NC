ad97f23e52e2d3f2fd947814efe6163ecd0556d4
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-10-18 16:02:58 +0530

    Update Rope.js
    Make these changes and check

--- Rope.js
+++ Rope.js
@@ -1,23 +1,24 @@
 class Rope{
     constructor(body1,body2,offsetX,offsetY)
     {
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
+        
         var options={
             bodyA: body1,
             bodyB: body2,
-            pointB:{x: this.offsetX,y: offsetY}
+            pointB:{x: offsetX,y: offsetY}
         }
-        console.log(options);
+       
         this.rope = Constraint.create(options);
         World.add(world,this.rope);
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
         
     }
     display(){
         var posA = this.rope.bodyA.position;
-        var posB = this.pointB;
+        var posB = this.rope.bodyB.position;
         strokeWeight(4);
-        line(posA.x,posA.y,posB.x,posB.y);      
+        line(pointA.x, pointA.y-20, pointB.x+this.offsetX, pointB.y);      
     }
 
 }
